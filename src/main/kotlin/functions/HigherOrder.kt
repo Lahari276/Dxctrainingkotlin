da02a64dc11c008ad7d9603ed523e437498c7c70
Lahari276 <laharireddy147@gmail.com>
2021-04-15 12:03:26 +0530

    Functions returning other functions

--- src/main/kotlin/functions/HigherOrder.kt
+++ src/main/kotlin/functions/HigherOrder.kt
@@ -6,13 +6,19 @@ fun mul(a: Int, b: Int):Int{//
     return a*b
 }
 fun sum(x: Int, y: Int) = x+y
+fun funReturningOtherFun(): (Int)->Int {
+    return ::square
+}
+fun square(x: Int) = x*x
 
 fun calculate(x: Int, y: Int, operation: (Int,Int) -> Int): Int{
     return operation(x,y)
 }
 fun main() {
+    val func = funReturningOtherFun()
     val resultSum = calculate(20,20, ::sum)
     val resultMul = calculate(60,18,::mul)
     println(resultSum)
     println(resultMul)
+    println(func(8))
 }
